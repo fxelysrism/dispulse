28e0a2155cc6b3271e542b2577eab2a6f883c75b
KIAN <elysrism@gmail.com>
2026-01-10 20:54:23 +0000

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dispulse",
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/dispulse/", // YOUR REPO NAME HERE
 });
