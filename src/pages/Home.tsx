70c225b5d6f5ff39b70564909f01b6f42aee31f0
KIAN <elysrism@gmail.com>
2026-01-11 19:32:13 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
-function Home() {
+const Home = () => {
   return (
     <div className="page-container">
-      <spline-viewer url="https://prod.spline.design/hJKMelfycWC4hWys/scene.splinecode"></spline-viewer>
+      {/* Desktop Version */}
+      <div className="desktop-view">
+        <spline-viewer url="https://prod.spline.design/hJKMelfycWC4hWys/scene.splinecode" />
+      </div>
+
+      {/* Mobile Version */}
+      <div className="mobile-view">
+        <div className="mobile-hero">
+          <h1>Dispulse</h1>
+          <p>We design brands that move culture.</p>
+          <button>Let’s Work</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
