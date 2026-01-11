2b865d0bc79942272999f52287f5b65f3b055765
KIAN <elysrism@gmail.com>
2026-01-11 19:37:39 +0000

    ffff

---
+++ src/types/spline.d.ts
@@ -0,0 +1,14 @@
+import type { CSSProperties } from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "spline-viewer": JSX.IntrinsicElements["spline-viewer"];
+    }
+  }
+}
+
+export {};
+
+
+export {};
